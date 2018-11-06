c5468d7479eb6e83883013846b08f433787bf473
felipethm <38351984+felipethm@users.noreply.github.com>
2018-11-06 16:20:52 -0200

    Add files via upload

--- untitled28.py
+++ untitled28.py
@@ -35,7 +35,7 @@ pygame.display.set_caption('Air Rescue')
 background = pygame.image.load("cartoon_clouds-wallpaper-800x600.png").convert()
 
 # Cria bola e adiciona em um grupo de Sprites.
-aviao = Aviao("Webp.net-resizeimage.png", 96, 110, randrange(1), randrange(1))
+aviao = Aviao("Webp.net-resizeimage.png", 250, 150, randrange(1), randrange(1))
 aviao_group = pygame.sprite.Group()
 aviao_group.add(aviao)
 
@@ -60,12 +60,12 @@ while rodando:
     if aviao.rect.x < -95:
         aviao.rect.x = 805
     if aviao.rect.x > 805:
-        aviao.vx = -95
+        aviao.rect.x = -95
 
     if aviao.rect.y < 0:
-        aviao.rect.y = 805
-    if aviao.rect.y > 805:
-        aviao.vy = -95
+        aviao.rect.y = 0
+    if aviao.rect.y > 490:
+        aviao.rect.y = 490
         
     keyinput = pg.key.get_pressed()
     
